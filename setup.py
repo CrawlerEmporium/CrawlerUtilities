9f25f9a13977d4d3f63532cb5b8cdd9eae517ba1
lorddusk <lang2619@gmail.com>
2021-06-24 16:55:37 +0200

    chore: version 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CrawlerUtilities",
-    version="0.0.3",
+    version="0.0.4",
     license="MIT",
     author="LordDusk",
     author_email="mail@crawleremporium.com",
