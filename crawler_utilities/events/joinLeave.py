d4fbc3016cbcbf6e79221330afe3e1543b4ee9ce
lorddusk <lang2619@gmail.com>
2021-06-27 22:51:13 +0200

    fix(joinLeave): Maybe this works better?

--- crawler_utilities/events/joinLeave.py
+++ crawler_utilities/events/joinLeave.py
@@ -13,39 +13,47 @@ class JoinLeave(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
+        if not guild.chunked:
+            await guild.chunk()
         tracker = await self.bot.fetch_channel(self.bot.tracking)
         embed = discord.Embed(color=Colour.green())
         embed.title = f"Joined Server"
         embed.description = f"{guild}"
-        if self.bot.user.id != 559331529378103317:
-            bots = sum(1 for m in guild.members if m.bot)
-            members = len(guild.members)
+        bots = sum(1 for m in guild.members if m.bot)
+        members = len(guild.members)
+        try:
             embed.add_field(name="Members", value=f"{members - bots}")
             embed.add_field(name="Bots", value=f"{bots}")
             embed.add_field(name="** **", value="** **")
-        embed.add_field(name="Owner", value=f"{guild.owner}")
-        embed.add_field(name="Mention", value=f"{guild.owner.mention}")
-        embed.add_field(name="Id", value=f"{guild.owner.id}")
-        embed.set_footer(text=f"{guild.id}")
+            embed.add_field(name="Owner", value=f"{guild.owner}")
+            embed.add_field(name="Mention", value=f"{guild.owner.mention}")
+            embed.add_field(name="Id", value=f"{guild.owner.id}")
+            embed.set_footer(text=f"{guild.id}")
+        except:
+            pass
         await tracker.send(embed=embed)
         await self.bot.change_presence(activity=discord.Game(f"with {len(self.bot.guilds)} servers | {self.bot.defaultPrefix}help | v{self.bot.version}"), afk=True)
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
+        if not guild.chunked:
+            await guild.chunk()
         tracker = await self.bot.fetch_channel(self.bot.tracking)
         embed = discord.Embed(color=Colour.red())
         embed.title = f"Left Server"
         embed.description = f"{guild}"
-        if self.bot.user.id != 559331529378103317:
-            bots = sum(1 for m in guild.members if m.bot)
-            members = len(guild.members)
+        bots = sum(1 for m in guild.members if m.bot)
+        members = len(guild.members)
+        try:
             embed.add_field(name="Members", value=f"{members - bots}")
             embed.add_field(name="Bots", value=f"{bots}")
             embed.add_field(name="** **", value="** **")
-        embed.add_field(name="Owner", value=f"{guild.owner}")
-        embed.add_field(name="Mention", value=f"{guild.owner.mention}")
-        embed.add_field(name="Id", value=f"{guild.owner.id}")
-        embed.set_footer(text=f"{guild.id}")
+            embed.add_field(name="Owner", value=f"{guild.owner}")
+            embed.add_field(name="Mention", value=f"{guild.owner.mention}")
+            embed.add_field(name="Id", value=f"{guild.owner.id}")
+            embed.set_footer(text=f"{guild.id}")
+        except:
+            pass
         await tracker.send(embed=embed)
         await self.bot.change_presence(activity=discord.Game(f"with {len(self.bot.guilds)} servers | {self.bot.defaultPrefix}help | v{self.bot.version}"), afk=True)
 
