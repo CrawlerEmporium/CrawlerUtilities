7a3a88113724c213cd48240bb887294f83eefd4c
lorddusk <lang2619@gmail.com>
2021-06-27 22:43:23 +0200

    fix(joinLeave): 5eCrawler cant check users on join, takes too long and errors out.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CrawlerUtilities",
-    version="0.0.7",
+    version="0.0.7a",
     license="MIT",
     author="LordDusk",
     author_email="mail@crawleremporium.com",
