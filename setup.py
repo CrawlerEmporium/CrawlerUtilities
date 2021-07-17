9d8d3f741ed2ee901bffc23255c057d27d302414
lorddusk <lang2619@gmail.com>
2021-07-17 13:37:27 +0200

    chore(version): bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CrawlerUtilities",
-    version="0.0.12",
+    version="0.0.13",
     license="MIT",
     author="LordDusk",
     author_email="mail@crawleremporium.com",
