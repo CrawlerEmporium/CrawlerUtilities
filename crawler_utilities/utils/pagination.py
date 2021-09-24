7d9c6114ad1de460337e34b56ab0bea02b124fa2
lorddusk <lang2619@gmail.com>
2021-09-24 18:48:51 +0200

    chore(update): updated githubcrawler to pycord

--- crawler_utilities/utils/pagination.py
+++ crawler_utilities/utils/pagination.py
@@ -3,11 +3,12 @@ from itertools import zip_longest
 import discord
 import asyncio
 
-from discord import ButtonStyle, Interaction
 from discord.ext import commands
 from copy import deepcopy
 from typing import List
 
+from discord.interactions import Interaction
+from discord.types.components import ButtonStyle
 from discord.ui import Button
 
 from .abc import Dialog
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CrawlerUtilities",
-    version="0.0.18",
+    version="0.0.19",
     license="MIT",
     author="LordDusk",
     author_email="mail@crawleremporium.com",
