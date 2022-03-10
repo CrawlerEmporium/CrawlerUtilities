e42c644aa8ed603e746d8f6dcb6c552c49e2251e
lorddusk <lang2619@gmail.com>
2022-03-10 11:39:42 +0100

    Load help commands in memory, to reduce db calls

--- crawler_utilities/cogs/help.py
+++ crawler_utilities/cogs/help.py
@@ -1,5 +1,7 @@
+import asyncio
+
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 from crawler_utilities.utils.embeds import ErrorEmbedWithAuthorWithoutContext
 from crawler_utilities.handlers import logger
@@ -13,15 +15,21 @@ log = logger.logger
 ID = {"CommunityCrawler": "discord", "5eCrawler": "5e", "5eCrawler.Nightly": "5e", "IssueCrawler": "issue"}
 
 
-async def get_help(ctx: discord.AutocompleteContext):
-    identifier = ID.get(ctx.bot.user.name)
-    helpCommands = await HELPDB.find({"bots": identifier, "disabled": None}).to_list(length=None)
-    return [helpC['command'] for helpC in helpCommands if ctx.value.lower() in helpC['command'].lower()]
-
-
 class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.reload_task.start()
+        self.commands = []
+
+    async def get_help(self, ctx: discord.AutocompleteContext):
+        return [command['command'] for command in self.commands if ctx.value.lower() in command['command'].lower()]
+
+    @tasks.loop(hours=1)
+    async def reload_task(self):
+        await self.bot.wait_until_ready()
+        log.info("[IN-MEMORY] Help commands")
+        identifier = ID.get(self.bot.user.name)
+        self.commands = await HELPDB.find({"bots": identifier, "disabled": None}).to_list(length=None)
 
     @commands.command(name="help")
     async def helpCommand(self, ctx):
@@ -49,64 +57,61 @@ class Help(commands.Cog):
     @slash_command(name="help")
     async def help(self, ctx, command: Option(str, "For which command do you want to see the help menu?", autocomplete=get_help)):
         """Get more information about the commands this bot has"""
-        identifier = ID.get(self.bot.user.name)
-        await getCommandHelp(ctx, command, identifier)
+        await self.getCommandHelp(ctx, command)
 
+    async def getCommandHelp(self, ctx, commandName):
+        helpCommand = next(command for command in self.commands if command['command'] == commandName)
+        prefix = '/'
+        if helpCommand is not None and not helpCommand.get('disabled', False):
+            description = helpCommand['description'][0]
+            embed = discord.Embed(title=f"{helpCommand['command'].title()}",
+                                  description=f"{description.replace('{/prefix}', prefix)}")
 
-async def getCommandHelp(ctx, command, identifier):
+            if helpCommand.get('syntax', None) is not None:
+                if len(helpCommand['syntax']) > 1:
+                    for i in range(len(helpCommand['syntax'])):
+                        embed.add_field(name=f"Syntax {i + 1}",
+                                        value=f"{helpCommand['syntax'][i].replace('{/prefix}', prefix)}", inline=False)
+                else:
+                    embed.add_field(name=f"Syntax", value=f"{helpCommand['syntax'][0].replace('{/prefix}', prefix)}",
+                                    inline=False)
 
-    helpCommand = await HELPDB.find_one({"bots": identifier, "command": command})
-    prefix = '/'
-    if helpCommand is not None and not helpCommand.get('disabled', False):
-        description = helpCommand['description'][0]
-        embed = discord.Embed(title=f"{helpCommand['command'].title()}",
-                              description=f"{description.replace('{/prefix}', prefix)}")
+            if helpCommand.get('aliasFor', None) is not None:
+                aliasString = ', '.join(helpCommand['aliasFor'])
+                embed.add_field(name=f"Aliases", value=f"{aliasString.replace('{/prefix}', prefix)}", inline=False)
 
-        if helpCommand.get('syntax', None) is not None:
-            if len(helpCommand['syntax']) > 1:
-                for i in range(len(helpCommand['syntax'])):
-                    embed.add_field(name=f"Syntax {i + 1}",
-                                    value=f"{helpCommand['syntax'][i].replace('{/prefix}', prefix)}", inline=False)
-            else:
-                embed.add_field(name=f"Syntax", value=f"{helpCommand['syntax'][0].replace('{/prefix}', prefix)}",
-                                inline=False)
-
-        if helpCommand.get('aliasFor', None) is not None:
-            aliasString = ', '.join(helpCommand['aliasFor'])
-            embed.add_field(name=f"Aliases", value=f"{aliasString.replace('{/prefix}', prefix)}", inline=False)
-
-        if helpCommand.get('options', None) is not None:
-            if len(helpCommand['options']) > 1:
-                for i in range(len(helpCommand['options'])):
-                    entryString = '\n'.join(helpCommand['options'][i]['entries'])
-                    embed.add_field(name=f"Option: {helpCommand['options'][i]['argument']}",
+            if helpCommand.get('options', None) is not None:
+                if len(helpCommand['options']) > 1:
+                    for i in range(len(helpCommand['options'])):
+                        entryString = '\n'.join(helpCommand['options'][i]['entries'])
+                        embed.add_field(name=f"Option: {helpCommand['options'][i]['argument']}",
+                                        value=f"{entryString.replace('{/prefix}', prefix)}",
+                                        inline=False)
+                else:
+                    entryString = '\n'.join(helpCommand['options'][0]['entries'])
+                    embed.add_field(name=f"Option: {helpCommand['options'][0]['argument']}",
                                     value=f"{entryString.replace('{/prefix}', prefix)}",
                                     inline=False)
-            else:
-                entryString = '\n'.join(helpCommand['options'][0]['entries'])
-                embed.add_field(name=f"Option: {helpCommand['options'][0]['argument']}",
-                                value=f"{entryString.replace('{/prefix}', prefix)}",
-                                inline=False)
 
-        if helpCommand.get('examples', None) is not None:
-            if len(helpCommand['examples']) > 1:
-                exampleString = "\n".join(helpCommand['examples'])
-                embed.add_field(name=f"Examples",
-                                value=f"{exampleString.replace('{/prefix}', prefix)}",
-                                inline=False)
-            else:
-                embed.add_field(name=f"Example", value=f"{helpCommand['examples'][0].replace('{/prefix}', prefix)}",
-                                inline=False)
+            if helpCommand.get('examples', None) is not None:
+                if len(helpCommand['examples']) > 1:
+                    exampleString = "\n".join(helpCommand['examples'])
+                    embed.add_field(name=f"Examples",
+                                    value=f"{exampleString.replace('{/prefix}', prefix)}",
+                                    inline=False)
+                else:
+                    embed.add_field(name=f"Example", value=f"{helpCommand['examples'][0].replace('{/prefix}', prefix)}",
+                                    inline=False)
 
-        typeString = ", ".join(helpCommand['types'])
-        embed.add_field(name=f"Types", value=f"{typeString}", inline=False)
+            typeString = ", ".join(helpCommand['types'])
+            embed.add_field(name=f"Types", value=f"{typeString}", inline=False)
 
-        permString = "Permissions: "
-        permString += ", ".join(helpCommand['permissions'])
-        embed.set_footer(text=permString)
-        return await ctx.respond(embed=embed)
-    else:
-        return await ctx.respond(f"I'm sorry, I couldn't find the help page for ``{command}``\nTry again later, or notify the bot developer through the support server.")
+            permString = "Permissions: "
+            permString += ", ".join(helpCommand['permissions'])
+            embed.set_footer(text=permString)
+            return await ctx.respond(embed=embed)
+        else:
+            return await ctx.respond(f"I'm sorry, I couldn't find the help page for ``{commandName}``\nTry again later, or notify the bot developer through the support server.")
 
 
 def setup(bot):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CrawlerUtilities",
-    version="0.0.30",
+    version="0.0.31",
     license="MIT",
     author="LordDusk",
     author_email="mail@crawleremporium.com",
