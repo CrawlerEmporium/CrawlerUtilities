84bd550e1facbe5e54e07b822e75abb3c3e1bad3
Tim Langen <tim.langen@driessen.nl>
2022-08-11 14:24:17 +0200

    (bump): version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CrawlerUtilities",
-    version="0.0.40",
+    version="0.0.41",
     license="MIT",
     author="LordDusk",
     author_email="mail@crawleremporium.com",
