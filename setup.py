e4a4d4fffbce8befa581bc1a9c09e1c935582621
lorddusk <lang2619@gmail.com>
2024-04-14 17:43:42 +0200

    Update py-cord requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CrawlerUtilities",
-    version="0.0.51",
+    version="0.0.52",
     license="MIT",
     author="LordDusk",
     author_email="mail@crawleremporium.com",
