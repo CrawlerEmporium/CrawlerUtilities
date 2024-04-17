31d01a8a4fb2f5a502dc9e719b09602e2d590427
lorddusk <lang2619@gmail.com>
2024-04-17 14:14:00 +0200

    Temp removal of requirements.txt for testing purposes of other bots

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CrawlerUtilities",
-    version="0.0.55",
+    version="0.0.56",
     license="MIT",
     author="LordDusk",
     author_email="mail@crawleremporium.com",
